f902affc22e0800f2f69f7c438437701efa0ab9f
Florian <f.follie@newquest.fr>
2021-09-30 17:14:23 +0200

    correction exemple

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 import { Rectangle, Circle, Point, Polygon} from './core/objectType.js';
 import { collider } from './core/colliderv2.js'
-
 const c = collider();
 
-
+let rect1 = Rectangle(0, 0 , 100, 100);
 let rect2 = Rectangle(50, 50 , 100, 100);
 
 let point1 = Point(120, 30);
 let point2 = Point(150, 30);
-
-let rect1 = Rectangle(0, 0 , 100, 100);
 
 let circle1 = Circle(70, 70, 50);
 let circle2 = Circle(50, 50, 50);
@@ -17,12 +14,6 @@ let circle2 = Circle(50, 50, 50);
 let pointd1 = Point(0, 50);
 let pointd2 = Point(100, 50);
 
-
 let polygon1 = c.convertrectangleToPolygon(rect1);
-
-let debugDiv = document.getElementById("debug");
-//debugDiv.innerHTML = c.pointRectangleCollision(point1, polygon1, true);
-
-//console.log('segment', Segment(point1.x, point1.y, point2.x, point2.y));
 
 console.log('CheckCollision', c.rectangleCircleCollision(rect1, circle1, true));
